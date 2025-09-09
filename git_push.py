0374768f5b99dddc568279f191fe98c212ab6ada
Muhammad Fajar Gumelar <mdjargum@gmail.com>
2025-09-09 14:04:10 +0700

    update add SSH Privet Key

--- git_push.py
+++ git_push.py
@@ -1,27 +1,24 @@
-import subprocess
 import os
+import subprocess
 from datetime import datetime
 
 def git_push(file_path, message=None):
-    """
-    Push file log baru ke GitHub (folder archives/).
-    file_path: path file CSV yang baru dibuat.
-    message: commit message (optional).
-    """
-
     if not message:
         message = f"Add log {os.path.basename(file_path)} at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+    
+    ssh_key = os.environ.get("SSH_PRIVATE_KEY")
+    if ssh_key:
+        # Tulis sementara ke file id_ed25519
+        ssh_path = "/tmp/id_ed25519"
+        with open(ssh_path, "w") as f:
+            f.write(ssh_key)
+        os.chmod(ssh_path, 0o600)
+        os.environ["GIT_SSH_COMMAND"] = f"ssh -i {ssh_path} -o StrictHostKeyChecking=no"
 
     try:
-        # Stage file
         subprocess.run(["git", "add", file_path], check=True)
-
-        # Commit
         subprocess.run(["git", "commit", "-m", message], check=True)
-
-        # Push
         subprocess.run(["git", "push", "origin", "master"], check=True)
-
         print(f"✅ Log {file_path} berhasil dipush ke GitHub")
     except subprocess.CalledProcessError as e:
         print("⚠️ Git push gagal:", e)
